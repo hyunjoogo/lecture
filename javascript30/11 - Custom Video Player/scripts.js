5c5387cf7f41cb251e8e3b9ac277a29ac9b92db2
Joo <hyunjoogo@gmail.com>
2021-01-12 03:24:17 +0900

    JS DAY 12,13

--- javascript30/11 - Custom Video Player/scripts.js
+++ javascript30/11 - Custom Video Player/scripts.js
@@ -94,5 +94,3 @@ ranges.forEach((input) => input.addEventListener("change", handleRanges));
 let mousedown = false;
 progress.addEventListener("click", scrub);
 progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
-progress.addEventListener("mousedown", () => (mousedown = true));
-progress.addEventListener("mouseup", () => (mousedown = false));
