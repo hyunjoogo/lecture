4b294e429012a0238d06ee1a50ede0ca783b9449
Joo <hyunjoogo@gmail.com>
2021-01-27 20:52:56 +0900

    TOC 추가 기능 완료

--- lifeCording-React/joo/src/App.js
+++ lifeCording-React/joo/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import TOC from "./components/TOC";
-import Content from "./components/Content";
+import ReadContent from "./components/ReadContent";
+import CreateContent from "./components/CreateContent";
 import Subject from "./components/Subject";
 import Control from "./components/Control";
 import "./App.css";
@@ -8,6 +9,7 @@ import "./App.css";
 class App extends Component {
   constructor(props) {
     super(props);
+    this.max_id = 3;
     this.state = {
       mode: "read",
       selected_content_id: 2,
@@ -23,10 +25,12 @@ class App extends Component {
   render() {
     console.log("App render");
     var _title,
-      _desc = null;
+      _desc,
+      _article = null;
     if (this.state.mode === "welcome") {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
     } else if (this.state.mode === "read") {
       var i = 0;
       while (i < this.state.contents.length) {
@@ -34,10 +38,25 @@ class App extends Component {
         if (data.id === this.state.selected_content_id) {
           _title = data.title;
           _desc = data.desc;
+          _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
           break;
         }
         i = i + 1;
       }
+    } else if (this.state.mode === "create") {
+      _article = (
+        <CreateContent
+          onSubmit={function (_title, _desc) {
+            this.max_id = this.max_id + 1;
+            const _contents = this.state.contents.concat({
+              id: this.max_id,
+              title: _title,
+              desc: _desc,
+            });
+            this.setState({ contents: _contents });
+          }.bind(this)}
+        ></CreateContent>
+      );
     }
     return (
       <div className="App">
@@ -64,7 +83,7 @@ class App extends Component {
             });
           }.bind(this)}
         ></Control>
-        <Content title={_title} desc={_desc}></Content>
+        {_article}
       </div>
     );
   }
