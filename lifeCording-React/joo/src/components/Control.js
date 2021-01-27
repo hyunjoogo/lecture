9d8dac7862b5c8a752b87586a08c21939424a2b9
Joo <hyunjoogo@gmail.com>
2021-01-27 18:56:52 +0900

    Click시 Mode 변경

---
+++ lifeCording-React/joo/src/components/Control.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+class Control extends Component {
+  render() {
+    console.log("Control render");
+    return (
+      <ul>
+        <li>
+          <a
+            href="/create"
+            onClick={function (e) {
+              e.preventDefault();
+              this.props.onChangeMode("create");
+            }.bind(this)}
+          >
+            Create
+          </a>
+        </li>
+        <li>
+          <a
+            href="/edit"
+            onClick={function (e) {
+              e.preventDefault();
+              this.props.onChangeMode("create");
+            }.bind(this)}
+          >
+            Edit
+          </a>
+        </li>
+        <li>
+          <input
+            type="button"
+            value="Delete"
+            onClick={function (e) {
+              e.preventDefault();
+              this.props.onChangeMode("create");
+            }.bind(this)}
+          ></input>
+        </li>
+      </ul>
+    );
+  }
+}
+export default Control;
