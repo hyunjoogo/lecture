d2696c04d40bc0770ff2bd536d326c890867d31f
Joo <hyunjoogo@gmail.com>
2021-01-28 16:49:37 +0900

    #2.2 Dynamic Component Generation

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,7 +1,51 @@
 import React from "react";
 
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+  },
+  {
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+  },
+  {
+    name: "Bibimbap",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+  },
+  {
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+  },
+  {
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+  },
+];
+
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h1>저는 {name}을 좋아합니다.</h1>
+      <img src={picture} alt="" />
+    </div>
+  );
+}
+
 function App() {
-  return <div> 안녕하세요! </div>;
+  return (
+    <div>
+      <h1>안녕하세요!</h1>
+      {foodILike.map((item) => (
+        <Food name={item.name} picture={item.image}></Food>
+      ))}
+    </div>
+  );
 }
 
 export default App;
