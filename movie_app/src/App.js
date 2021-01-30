4f4aabafe3d3b98373d31d0f2f8e081ae32395b8
hyunjoogo <hyunjoogo@gmail.com>
2021-01-30 22:53:34 +0900

    Add : 숫자 카운팅 기능 추가
    - 더하기 / 빼기/ 리셋 / +2 버튼 추가
    - 현재 숫자 표현

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,74 +1,52 @@
 import React from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 
-const foodILike = [
-  {
-    id: 1,
-    name: "kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rate: 5.0,
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rate: 4.5,
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rate: 4.0,
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rate: 4.6,
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rate: 4.2,
-  },
-];
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+  add = () => {
+    this.setState((current) => ({
+      count: current.count + 1,
+    }));
+    console.log(this.state.count);
+  };
 
-function Food({ name, picture, rate }) {
-  return (
-    <div>
-      <h1>저는 {name}을 좋아합니다.</h1>
-      <img src={picture} alt="" />
-      <h3>{rate}/5.0</h3>
-    </div>
-  );
-}
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rate: PropTypes.number,
-};
+  reset = () => {
+    this.setState({ count: 0 });
+  };
 
-function App() {
-  return (
-    <div>
-      <h1>안녕하세요!</h1>
-      {foodILike.map((item) => (
-        <Food
-          key={item.id}
-          name={item.name}
-          picture={item.image}
-          rate={item.rate}
-        ></Food>
-      ))}
-    </div>
-  );
+  incrementCount = () => {
+    // 이거 이유는 좀 알아야 겠다. 비동기 개념이 나온다.
+    this.setState((state) => {
+      return { count: state.count + 1 };
+    });
+  };
+
+  handleSomething = () => {
+    // 그리고 this.add()를 해도 안되네? 왜???
+    this.add();
+    this.add();
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>이 숫자는 {this.state.count}입니다.</h1>
+        <button onClick={this.add}>더하기</button>
+        <button onClick={this.minus}>빼기</button>
+        <button onClick={this.reset}>리셋</button>
+        <button onClick={this.handleSomething}>+2</button>
+      </div>
+    );
+  }
 }
 
 export default App;
