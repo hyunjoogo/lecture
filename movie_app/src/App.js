77527d9c55fc3f193e705398f2eae77a11cde69d
hyunjoogo <hyunjoogo@gmail.com>
2021-01-31 15:06:16 +0900

    Add : fecthing 기능 추가

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
-// import PropTypes from "prop-types";
+import axios from "axios";
 
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1></h1>
-      </div>
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  getMovies = async () => {
+    const movies = await axios.get(
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json"
     );
+  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading.." : "We are ready"}</div>;
   }
 }
 
