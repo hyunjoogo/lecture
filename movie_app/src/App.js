8a8805b3b0c4df133755973a762d9cc11598ee62
hyunjoogo <hyunjoogo@gmail.com>
2021-02-01 02:09:06 +0900

    Add : Movie CSS / App CSS 완료

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./app.css";
 
 class App extends React.Component {
   state = {
@@ -24,24 +25,24 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
+      <section className="movies">
         {isLoading
           ? "로딩중.."
           : movies.map(movie => {
               return (
                 <Movie
                   key={movie.id}
-                  id={movie.id}
                   title={movie.title}
                   year={movie.year}
                   rating={movie.rating}
                   runtime={movie.runtime}
                   summary={movie.summary}
                   poster={movie.medium_cover_image}
+                  genres={movie.genres}
                 />
               );
             })}
-      </div>
+      </section>
     );
   }
 }
--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -1,18 +1,43 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({ id, title, year, rating, runtime, summary, poster }) {
-  return <h5>{title}</h5>;
+function Movie({ title, year, rating, runtime, summary, poster, genres }) {
+  return (
+    <div className="movie__data">
+      <img src={poster} alt={title} title={title} className="movie__poster" />
+      <div className="movie__info">
+        <h3 className="movie__title">{title}</h3>
+        <p>장르</p>
+        <ul className="movie__genres">
+          {genres.map((genre, index) => {
+            return (
+              <li key={index} className="genres__genre">
+                {genre}
+              </li>
+            );
+          })}
+        </ul>
+        <p className="movie__year">개봉 : {year}</p>
+        <p className="movie__runtime">상영시간 : {runtime}분</p>
+        <p className="movie__rating">평점 : {rating}</p>
+        <div className="movie__summary">
+          <p>줄거리</p>
+          <p className="summary__text">{summary.slice(0, 140)}...</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 Movie.propTypes = {
-  id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   year: PropTypes.number.isRequired,
   rating: PropTypes.number.isRequired,
   runtime: PropTypes.number.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
