a50f15b9b8b9069e9cf2a52117d01e52ba9d2309
hyunjoogo <hyunjoogo@gmail.com>
2021-02-11 18:08:36 +0900

    Add : CASS 버튼 사이즈/색상/옵션 관리

--- Velopert-React/styling-with-sass/src/components/Button.js
+++ Velopert-React/styling-with-sass/src/components/Button.js
@@ -2,12 +2,20 @@ import React from "react";
 import classNames from "classnames";
 import "./Button.scss";
 
-const Button = ({ children, size }) => {
-  return <button className={classNames("Button", size)}>{children}</button>;
+const Button = ({ children, size, color, outline, fullWidth, ...rest }) => {
+  return (
+    <button
+      className={classNames("Button", size, color, { outline, fullWidth })}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
 };
 
 Button.defaultProps = {
   size: "medium",
+  color: "blue",
 };
 
 export default Button;
