a3cef09418b731da5d09b7ceed0c561a790a0ac9
hyunjoogo <hyunjoogo@gmail.com>
2021-02-15 22:22:43 +0900

    HTML 공부

---
+++ father/HTML/6,7장/index.js
@@ -0,0 +1,19 @@
+const element = document.getElementsByTagName("title");
+const element2 = document.querySelector("title");
+
+console.log("getElementsByTagName");
+console.log(element);
+
+console.log("getElementsByTagName.item[0]");
+console.log(element[0]);
+
+console.log("querySelector");
+console.log(element2);
+
+// console.log("변경전", element.text);
+
+// element.text = "제목 변경";
+// console.log("변경 후", element.text);
+
+// console.log("textContent", element.textContent);
+// console.log("document.title", document.title);
